10dcbe535c84864085556d67a152662f043d07cb
Reawpai Chunsoi <reawpai.chun@yahoo.com>
2020-10-24 02:33:22 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let name: String
+</script>
+
+<style type="scss">
+  $color: blue;
+
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: $color;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
